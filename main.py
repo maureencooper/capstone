559a8308609701dd6c61b590a9d13eea50043626
maureencooper <maureen.cooper@uc.cl>
2022-03-28 23:50:55 -0300

    add get_top_10_active_users

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ def get_top_10_retweets(dataframe):
     df = dataframe.copy()
 
     return df.sort_values('retweetCount', ascending=False)['retweetCount'].head(10)
+
+
+def get_top_10_active_users(dataframe):
+    users = pd.json_normalize(dataframe['user'])
+    users = pd.DataFrame(users)
+
+    return users['id'].value_counts()[:10].sort_values(ascending=False)
