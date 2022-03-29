5f5c653b718abace37119040558b587bf9f5e410
maureencooper <maureen.cooper@uc.cl>
2022-03-28 23:49:28 -0300

    add get_top_10_retweets

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 
 def get_dataframe(json_path):
     return pd.read_json(json_path, lines=True)
+
+
+def get_top_10_retweets(dataframe):
+    df = dataframe.copy()
+
+    return df.sort_values('retweetCount', ascending=False)['retweetCount'].head(10)
