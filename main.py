f8b06dce17bbaf50d06658b4b06da0474dd1c822
maureencooper <maureen.cooper@uc.cl>
2022-03-28 23:52:00 -0300

    add get_top_10_dates

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ def get_top_10_active_users(dataframe):
     users = pd.DataFrame(users)
 
     return users['id'].value_counts()[:10].sort_values(ascending=False)
+
+
+def get_top_10_dates(dataframe):
+    df = dataframe.copy()
+    df['date'] = pd.to_datetime(df['date'], format='%y%m%d')
+
+    return df['date'].value_counts()[:10].sort_values(ascending=False)
